b45d316143cb3a66d3d5f7f24ba4350559a924f0
waipyae <waipyae99@gmail.com>
2022-01-01 00:03:21 +0630

    js var 0.1

--- script.js
+++ script.js
@@ -4,4 +4,30 @@ function computerPlay(){
     return rps_array[random_rps];
 }
 
-console.log(computerPlay());
+function playGround(playerSelection, computerSelection){
+    let pSelection = playerSelection.toLowerCase();
+    let comSelection = computerSelection.toLowerCase();
+    if(pSelection == 'rock' && comSelection == 'paper'){
+        return "You Lose! Paper beats Rock";
+    }else if(pSelection == 'rock' && comSelection == 'scissors'){
+        return "You Win! Rock beats Scissors";
+    }else if(pSelection == 'paper' && comSelection == 'rock'){
+        return "You Win! Paper beats Rock";
+    }else if(pSelection == 'paper' && comSelection == 'scissors'){
+        return "You Lose! Scissors beats Paper";
+    }else if(pSelection == 'scissors' && comSelection == 'rock'){
+        return "You Lose! Rock beats Scissors";
+    }else if(pSelection == 'scissors' && comSelection == 'paper'){
+        return "You Win! Scissors beats Paper";
+    }else{
+        return "Tie!";
+    }
+}
+
+let computerSelection = computerPlay();
+let playerSelection = prompt("Choose one among Rock , Paper and Scissors");
+
+let result = playGround(playerSelection, computerSelection);
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(result);
